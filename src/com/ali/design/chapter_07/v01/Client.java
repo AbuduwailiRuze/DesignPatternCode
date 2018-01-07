394efad56034c40d85a39592e710d55a7ab4fd62
Waili <l>
2018-01-07 21:02:30 +0800

    111

--- src/com/ali/design/chapter_07/v01/Client.java
+++ src/com/ali/design/chapter_07/v01/Client.java
@@ -3,7 +3,7 @@ package com.ali.design.chapter_07.v01;
 public class Client {
 	//阿力的学习笔记
 	public static void main(String[] args) {
-		//代理模式客户端
+		//代理模式客户端调用
 		Proxy proxy=new Proxy();
 		proxy.request();
 	}
